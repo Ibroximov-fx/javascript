5392769bc8faf2dbf3f81a753b9d8bc0c0dbf39b
ibroximov-fx <ibrohimovahmad91@gmail.com>
2025-05-08 02:57:54 -0700

    java

---
+++ dars1/main.js
@@ -0,0 +1,36 @@
+// let yil = 2025
+// let yoshi = 25
+// console.log(yil-yoshi)
+
+
+// let yosh = 34
+// let a ="kun"
+// console.log(yosh*365+a)
+// let b ="soat"
+// console.log(yosh*365*24+b)
+// let c ="daqiqa"
+// console.log(yosh*365*24*60+c)
+
+
+// let a = 8
+// let b = 10
+// console.log(a-b)
+// console.log(a+b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+
+
+// let a = "ibrohimov "
+// let b = "ahmad"
+// console.log(a+b)
+
+
+// let usd =100
+// let sum = 12800
+// console.log(usd*sum)
+
+
+// let yosh= "41"
+// let raqam = Number (yosh)
+// console.log(raqam)
