4c17a84c81741ef0958b058e9896dfa3abfbd8eb
ibroximov-fx <ibrohimovahmad91@gmail.com>
2025-05-30 05:04:00 -0700

    git remote add origin https://github.com/Ibroximov-fx/imtihon.git git branch -M main git push -u origin main

---
+++ imtihon/main.js
@@ -0,0 +1,65 @@
+let a ="bir"
+let b = "ikki"
+let c = "bush"
+a=c
+b=a
+c=a
+
+
+console.log(a)
+console.log(b)
+
+
+
+
+
+
+
+
+// let baho = prompt("baho quying")*1
+// if (baho >= 90 &&  baho <= 100) {
+//     console.log("A")
+// }else if (baho >= 80 &&  baho <= 89) {
+//     console.log("B")
+// }else if (baho >= 70 &&  baho <= 79) {
+//     console.log("C")
+// }else if (baho >= 60 &&  baho <=69) {
+//     console.log("D")
+// }else if (baho >= 0 &&  baho <= 59) {
+//     console.log("F")
+// }else {
+//     console.log("Bunaqa baho quyib bulmaydi")
+// }
+
+
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+// let a = prompt ("son kiriting")
+// console.log(a*a)
+
+
+
+
+
+
+// let user = {
+//     name:"ahmad",
+//     age:"16",
+//     surname:"ibrohimov",
+// }
+// console.log(user);
+
+
+
+
